9164ff1165dd17828ff5ef864825bd680882d259
Jacquiiii <jacqui.koroll@telus.com>
2022-11-23 15:31:38 -0800

    added setupInput & handleUserInput to play.js

--- client.js
+++ client.js
@@ -14,15 +14,14 @@ const connect = function () {
   // conn.on("connect", (data) => {
   //   console.log("you ded cuz you idled");
   // });
-
-  // interpret incoming data as text
+  
   conn.setEncoding("utf8");
 
   return conn;
 };
 
 module.exports = {
-  connect
+  connect,
 };
 
 
