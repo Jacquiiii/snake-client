e2fa651c1bb145667ef869f8957da50422ee0763
Jacquiiii <jacqui.koroll@telus.com>
2022-11-23 15:01:32 -0800

    saving changes after testing move

--- client.js
+++ client.js
@@ -1,13 +1,20 @@
 // establishes a connection with the game server
+const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
-    port: 50541
+    port: 50541,
   });
 
   conn.on("connect", (data) => {
-    console.log("you ded cuz you idled");
+    console.log("Successfully connected to game server");
+    conn.write("Name: JK");
+    conn.write("Move: up");
   });
+
+  // conn.on("connect", (data) => {
+  //   console.log("you ded cuz you idled");
+  // });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -18,3 +25,11 @@ const connect = function () {
 module.exports = {
   connect
 };
+
+
+
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
